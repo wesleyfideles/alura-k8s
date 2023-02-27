6fd760e7979114e24b66a2c08aec7eed4b24e2dc
Wesley Fideles <wesley.fideles@outlook.com>
2023-02-27 15:32:17 -0300

    Atualização dos arquivos .sh
    Atualização dos arquivos .sh

--- Curso k8s na Alura/delete.sh
+++ Curso k8s na Alura/delete.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
-echo -e "\e[31;1m Deletando todos arquivos .txt do diretório atual:\e[m \e[36;1m $PWD \e[m"
+echo -e "\e[31;1m Deletando todos arquivos .txt do diretório atual:\e[m $PWD"
 rm *.txt
 echo -e "\e[31;1m Deletando todos os serviços k8s \e[m"
 sleep 5
 kubectl delete -f . 
 sleep 10
-echo -e "\e[31;1m Todos arquivos .yml do diretório atual foram deletados:\e[m \e[36;1m $PWD \e[m"
+echo -e "\e[31;1m Todos arquivos .yml e .txt do diretório atual foram deletados:\e[m $PWD"
